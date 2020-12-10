180b6f55806d958470a1e0fd7e2545c17b2192ec
Jonathan Falvey <55098346+JDirac@users.noreply.github.com>
2020-12-10 16:38:08 +0000

    Removed unused methods in Address.java

--- source/Address.java
+++ source/Address.java
@@ -15,29 +15,4 @@ public class Address {
     public String toString() {
         return street + ", " + city + ", " + county;
     }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCounty() {
-        return county;
-    }
-
-    public void setCounty(String county) {
-        this.county = county;
-    }
 }
-
