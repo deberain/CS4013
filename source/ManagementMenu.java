518edea1b1fb098b88288b601f56abf2ab9a36a8
Jonathan Falvey <55098346+JDirac@users.noreply.github.com>
2020-12-10 16:46:36 +0000

    Update ManagementMenu.java
    Implemented the final functions for the CLI interface. Work still needs to be done for reading in data from csv files and writing to them

--- source/ManagementMenu.java
+++ source/ManagementMenu.java
@@ -1,13 +1,22 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ManagementMenu {
     Scanner scan = new Scanner(System.in);
-
+    PropertyOwnersList owners = new PropertyOwnersList();
+    
+    //READ IN DATA HERE
+    
+    int yearTaxLastCalculated = 2019;
+    
     public void run() {
-        PropertyOwnersList owners = new PropertyOwnersList();
+        if (LocalDate.now().getYear() > yearTaxLastCalculated) {
+            owners.calculateAllTax();
+            yearTaxLastCalculated = LocalDate.now().getYear();
+        }
+        
         boolean more = true;
-
 
         while(more) {
             System.out.println("P)roperty tax payment data     O)verdue property tax     S)tatistics     E)xperiment with tax rates      Q)uit");
@@ -30,13 +39,46 @@ public class ManagementMenu {
                 }
             }
             else if (command.equals("O")) {
+                System.out.println("Please enter the year you want data from: ");
+                int year = scan.nextInt();
+                System.out.println("Get data from : A)ll property owners    P)roperty owners from particular area");
+                command = scan.nextLine().toUpperCase();
 
+                if (command.equals("A")) {
+                    owners.getAllOverdueTax(year);
+                } else if(command.equals("P")) {
+                    System.out.println("Enter area to check (eircode format): ");
+                    String area = scan.nextLine();
+
+                    owners.getAllOverdueTax(year, area);
+                }
             }
             else if (command.equals("S")) {
+                System.out.println("Enter area you would like to receive statistics for (eircode format): ");
+                String area =  scan.nextLine();
 
+                System.out.println(owners.getStatistics(area));
             }
             else if (command.equals("E")) {
+                System.out.println("Enter a Value for the flat charge: ");
+                double flat = scan.nextDouble();
 
+                System.out.println("Enter a value for the rate for market values between 150,000 and 400,000 (0.xx): ");
+                double rate1 = scan.nextDouble();
+
+                System.out.println("Enter a value for the rate for market values between 400,000 and 650,000 (0.xx): ");
+                double rate2 = scan.nextDouble();
+
+                System.out.println("Enter a value for the rate for market values above 650,000 (0.xx): ");
+                double rate3 = scan.nextDouble();
+
+                System.out.println("Enter Values for location tax (city, large town, small town, village, countryside)");
+                String location = scan.nextLine();
+
+                System.out.println("Enter a value for Non principle private residence tax: ");
+                double ppr = scan.nextDouble();
+
+                owners.compareRevenue(flat, rate1, rate2, rate3, location, ppr);
             }
             else if (command.equals("Q")) {
                 more = false;
