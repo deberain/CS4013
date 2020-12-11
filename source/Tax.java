f69d2e4f8b06ff59e6dea1637841ee6080b94a3d
Jonathan Falvey <55098346+JDirac@users.noreply.github.com>
2020-12-11 18:53:43 +0000

    Update Tax.java
    Added constructors necessary to assist Denis' code to read in sample data. Also added documentation for each method in the Tax class

--- source/Tax.java
+++ source/Tax.java
@@ -1,5 +1,10 @@
 import java.time.LocalDate;
 
+/**
+ * 
+ * Tax class created by Bryan Carty to represent a tax due for a property and track key information
+ * 
+ */
 public class Tax {
 	int yearDue;
 	double taxDue;
@@ -8,10 +13,24 @@ public class Tax {
 	boolean paid = false;
 	double valueWhenPaid = 0;
 	int yearPaid = 9999;
-	
+
+	/**
+	 * 
+	 * No-arg constructor for Tax class
+	 * 
+	 */
 	public Tax() {}
 
-	//when passing date to this class use LocalDate date = LocalDate.of(2020, 1, 8)
+	/**
+	 * 
+	 * Constructor used when creating a new tax
+	 * 
+	 * Author: Bryan Carty
+	 * 
+	 * @param propOwner owner of the property
+	 * @param prop property for which tax is due
+	 * @param taxDue amount that must be paid
+	 */
 	public Tax(PropertyOwner propOwner, Property prop, double taxDue) {
 		this.propOwner = propOwner;
 		this.prop = prop;
@@ -19,24 +38,80 @@ public class Tax {
 
 		yearDue = LocalDate.now().getYear(); //tax must be paid this year.
 	}
-	
+
+	/**
+	 * 
+	 * Constructor to be used when reading in sample data
+	 * 
+	 * Author: Jonathan Falvey
+	 * 
+	 * @param owner Owner of the property
+	 * @param property property that tax is due on
+	 * @param taxDue amount that is due to be paid
+	 * @param yearDue the year in which this tax must be paid
+	 */
+	public Tax(PropertyOwner owner, Property property, double taxDue, int yearDue) {
+		propOwner = owner;
+		prop = property;
+		this.taxDue = taxDue;
+		this.yearDue = yearDue;
+	}
+
+	/**
+	 *
+	 * Alternate Constructor to be used when reading in sample data. Used for taxes that have been paid
+	 * 
+	 * Author: Jonathan Falvey
+	 *
+	 * @param owner Owner of the property
+	 * @param property property that tax is due on
+	 * @param taxDue amount that is due to be paid
+	 * @param yearDue the year in which this tax must be paid
+	 * @param paid indicates if this tax has been paid
+	 * @param valueWhenPaid the amount that was paid (incl. penalty rate if applicable) when the owner paid this tax
+	 * @param yearPaid the year in which this tax was paid                        
+	 */
+	public Tax(PropertyOwner owner, Property property, double taxDue, int yearDue, boolean paid, double valueWhenPaid, int yearPaid) {
+		propOwner = owner;
+		prop = property;
+		this.taxDue = taxDue;
+		this.yearDue = yearDue;
+		this.paid = paid;
+		this.valueWhenPaid = valueWhenPaid;
+		this.yearPaid = yearPaid;
+	}
+
+
+	/**
+	 * Author: Bryan Carty
+	 * 
+	 * @return returns the year that this tax should be paid within
+	 */
 	public int getYearDue() {
 		return this.yearDue;
 	}
-	
-	public void setYearDue(int yearDue) {
-		this.yearDue = yearDue;
-	}
-	
-	public Property getProperty() {
-		return this.prop;
-	}
 
+	/**
+	 * 
+	 * Method called when a tax is paid. This updates the tax, indicating that it has been paid and how much was paid
+	 * 
+	 * Author: Jonathan Falvey
+	 * 
+	 * @param amount amount that was paid
+	 */
 	public void taxPaid(double amount) {
 		paid = true;
 		valueWhenPaid = amount;
 	}
 
+	/**
+	 * 
+	 * Used when the tax is overdue to be paid. It gets the current value of the tax including the penalty rates
+	 * 
+	 * Author: Jonathan Falvey
+	 * 
+	 * @return returns the current value of the tax
+	 */
 	public double getCurrentValue() {
 		double tempTax = taxDue;
 		int yearsNotPaid = LocalDate.now().getYear() - yearDue;
@@ -49,6 +124,15 @@ public class Tax {
 		return tempTax;
 	}
 
+	/**
+	 * 
+	 * Gets the value of a tax during a specific year
+	 * 
+	 * Author: Jonathan Falvey
+	 * 
+	 * @param year the year we would like to find the value of the tax in
+	 * @return returns the value of the tax during the specified year
+	 */
 	public double getValue(int year) {
 		double tempTax = taxDue;
 		int yearsNotPaid = year - yearDue;
@@ -61,15 +145,40 @@ public class Tax {
 		return tempTax;
 	}
 
+	/**
+	 * 
+	 * For presentation to property owner
+	 * 
+	 * Author: Bryan Carty
+	 * 
+	 * @return returns a convenient string format representation with a few details. 
+	 */
 	public String format() {
 		return yearDue + ", Current tax due: " + getCurrentValue();
 	}
 
+
+	/**
+	 * 
+	 * For presentation to the Department of Environment
+	 * 
+	 * Author: Bryan Carty
+	 * 
+	 * @param year the year we would like to see tax values for
+	 * @return returns a conveneient string format representation of the tax with some key details
+	 */
 	public String managementFormat(int year) {
 		return propOwner.getName() + ", Tax value during year: " + getValue(year) + ", Property: " + prop.format();
 	}
 
-
+	/**
+	 * 
+	 * General String format of a Tax.
+	 * 
+	 * Author: Jonathan Falvey
+	 * 
+	 * @return returns an apporpriate string format representation depending on whether the tax has been paid or not.
+	 */
 	public String toString() {
 		String temp = "";
 		if (paid) {
@@ -81,7 +190,3 @@ public class Tax {
 	}
 
 }
-
-
-
-
