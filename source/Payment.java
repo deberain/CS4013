61616fe9ccd6112a95e2f3d6b1e39b8cc6363b1c
Jonathan Falvey <55098346+JDirac@users.noreply.github.com>
2020-12-12 14:15:59 +0000

    Update Payment.java

--- source/Payment.java
+++ source/Payment.java
@@ -1,3 +1,10 @@
+/**
+ *
+ * Class created to represent a receipt for tax payment on a property
+ *
+ * Author: Jonathan Falvey
+ *
+  */
 public class Payment {
     private PropertyOwner owner;
     private Property property;
@@ -5,8 +12,23 @@ public class Payment {
     private double amountPaid;
     private double taxRemaining;
 
+    /**
+     *
+     * Default no-arg constructor
+     *
+     */
     public Payment() {}
 
+    /**
+     *
+     * Constructor used to crate new payments
+     *
+     * @param owner the owner who has paid tax
+     * @param property the property tax has been paid on
+     * @param year the year this payment was made
+     * @param amountPaid amount of tax that was paid
+     * @param taxRemaining how much tax the owner must still pay
+     */
     public Payment(PropertyOwner owner, Property property, int year, double amountPaid, double taxRemaining) {
         this.owner = owner;
         this.property = property;
@@ -15,19 +37,33 @@ public class Payment {
         this.taxRemaining = taxRemaining;
     }
 
+    /**
+     *
+     * Presents key information about the payment for property owners
+     *
+     * @return returns a convenient String format of the property
+     */
     public String ownerFormat() {
         return "Year: " + year + ", Property: " + property.getAddress() + ", Amount paid: " + amountPaid + ", Balance Remaining: " + taxRemaining;
     }
 
+    /**
+     *
+     * Presents key information about the payment for properties
+     *
+     * @return returns a convenient String format of the property
+     */
     public String propertyFormat() {
         return "Year: " + year + ", Owner: " + owner.getName() + ", Amount Paid: " + ", Tax Remaining: " + taxRemaining;
     }
 
-    public double getBalance() {
-        return taxRemaining;
-    }
-
-    public int getYear() {
-        return year;
+    /**
+     *
+     * summaries information about the payment for writing to a csv file
+     *
+     * @return csv line String
+     */
+    public String writeFormat() {
+        return "Payment," + year + "," + amountPaid + "," + taxRemaining +"\n";
     }
 }
